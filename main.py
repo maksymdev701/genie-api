3ac0887cc397af0b507bbc7bd3f9f8c308e1215f
maksymdev701 <mmeliukh71@gmail.com>
2023-08-01 02:43:38 +0300

    commit for eb deploy

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from config import settings
 
 app = FastAPI()
 
-origins = [settings.CLIENT_ORIGIN]
+origins = [settings.CLIENT_ORIGIN, "http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
