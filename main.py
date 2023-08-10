b9477d55b7399c1c5965141aa74ef27b1efbfe37
maksymdev701 <mmeliukh71@gmail.com>
2023-08-10 11:04:03 +0300

    export and import api

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import openai
 
-from routers import products, prompts, auth, user
+from routers import products, prompts, auth, user, genie
 from config import settings
 import os
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 origins = [settings.CLIENT_ORIGIN, "https://main.d1l17mtvn7i9t6.amplifyapp.com"]
 
@@ -24,6 +27,7 @@ app.include_router(products.router, prefix="/api/products", tags=["Products"])
 app.include_router(prompts.router, prefix="/api/prompts", tags=["Prompts"])
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(user.router, prefix="/api/users", tags=["Users"])
+app.include_router(genie.router, prefix="/api/genie", tags=["Genie"])
 
 
 @app.get("/")
